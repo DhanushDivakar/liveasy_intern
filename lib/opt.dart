1982f4167e3d76ab7ccf8fb52f2c67aaead36f82
DhanushDivakar <93920313+DhanushDivakar@users.noreply.github.com>
2022-08-28 19:39:33 +0530

    fourth

--- lib/opt.dart
+++ lib/opt.dart
@@ -16,116 +16,114 @@ class _otpState extends State<otp> {
       body: SizedBox(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 100,
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 100,
+            ),
+            const Text(
+              "Verify Phone",
+              style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              "Code sent to 89898889898",
+              style: const TextStyle(
+                  color: Colors.black54,
+                  fontSize: 16,
+                  fontWeight: FontWeight.normal),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 25, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  _textFieldOTP(first: true, last: false),
+                  _textFieldOTP(first: false, last: false),
+                  _textFieldOTP(first: false, last: false),
+                  _textFieldOTP(first: false, last: false),
+                  _textFieldOTP(first: false, last: false),
+                  _textFieldOTP(first: false, last: true),
+                ],
               ),
-              const Text(
-                "Verify Phone",
-                style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Text(
-                "Code sent to 89898889898",
-                style: const TextStyle(
-                    color: Colors.black54,
-                    fontSize: 16,
-                    fontWeight: FontWeight.normal),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 25, 0, 0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    _textFieldOTP(first: true, last: false),
-                    _textFieldOTP(first: false, last: false),
-                    _textFieldOTP(first: false, last: false),
-                    _textFieldOTP(first: false, last: false),
-                    _textFieldOTP(first: false, last: false),
-                    _textFieldOTP(first: false, last: true),
-                  ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Didn’t receive the code? ",
+                  style: TextStyle(
+                      color: Colors.black54,
+                      fontWeight: FontWeight.normal,
+                      fontSize: 16),
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    "Didn’t receive the code? ",
+                GestureDetector(
+                  onTap: () {
+                    // Navigator.push(
+                    //     context,
+                    //     MaterialPageRoute(
+                    //         builder: (context) =>
+                    //             SignUpScreen()));
+                  },
+                  child: Text(
+                    "Request Again",
                     style: TextStyle(
-                        color: Colors.black54,
-                        fontWeight: FontWeight.normal,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
                         fontSize: 16),
                   ),
-                  GestureDetector(
-                    onTap: () {
-                      // Navigator.push(
-                      //     context,
-                      //     MaterialPageRoute(
-                      //         builder: (context) =>
-                      //             SignUpScreen()));
-                    },
-                    child: Text(
-                      "Request Again",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.w500,
-                          fontSize: 16),
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 25,
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(36, 0, 36, 0),
-                child: Container(
-                  height: 50,
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      foregroundColor:
-                          MaterialStateProperty.all<Color>(Colors.white),
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          Color.fromRGBO(46, 59, 98, 1)),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(0),
-                        ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(36, 0, 36, 0),
+              child: Container(
+                height: 50,
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        Color.fromRGBO(46, 59, 98, 1)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(0),
                       ),
                     ),
-                    child: Text(
-                      "VERIFY AND CONTINUE",
-                      style: TextStyle(fontSize: 18),
-                    ),
+                  ),
+                  child: Text(
+                    "VERIFY AND CONTINUE",
+                    style: TextStyle(fontSize: 18),
                   ),
                 ),
               ),
-              Stack(
-                alignment: Alignment.bottomCenter,
-                children: [
-                  Positioned(
-                      bottom: 0,
-                      child: Container(
-                       // width: double.infinity,
-                       // height: 50,
-                        child: Image.asset('assets/Vector.png'),
-                      )),
-                ],
-              )
-            ],
-          ),
+            ),
+
+            // Stack(
+            //   children: [
+            //     Positioned(
+            //         bottom: 0,
+            //         child: Container(
+            //          // width: double.infinity,
+            //          // height: 50,
+            //           child: Image.asset('assets/Vector.png'),
+            //         )),
+            //   ],
+            // )
+          ],
         ),
       ),
     );
